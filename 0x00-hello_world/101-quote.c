d06e6c6fc546bf3b396c0d20b3432e97534434ee
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-16 16:49:42 -0700

    commit changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - The entry point
+ *
+ * Return: 1 at the end
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"-Dora Korpar,2015-10-19\n");
+	return (1);
+}
