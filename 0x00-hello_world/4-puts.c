151e1abd92bc1307d8c423a7507cc21ebcad3fac
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-16 15:19:16 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,8 @@
-/*
- * file name: 4-puts.c
- *
- */
-
 #include <stdio.h>
 
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle
- *
- * return: always 0
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
