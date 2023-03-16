91a11f2408591ef433aaa6405fdeecec4848c4ab
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-16 15:32:03 -0700

    print a string

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * return: 0 Always
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
