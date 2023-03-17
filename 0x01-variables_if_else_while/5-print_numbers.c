b201c53662decc174f1e78970c02bdcb9dec4e83
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-17 15:25:25 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase except q and e followed by a new line
+ * Return: 0 if it's true
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
