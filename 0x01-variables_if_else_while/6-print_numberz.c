35ff2f3db42d4f10ebc1453a6af081e6aaca327a
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-17 15:40:56 -0700

    print base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints 
+ * Return: 0 if it's true
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i < '10'; i++)
+		putchar(i);
+	putchar(\n);
+	return (0):
+}
