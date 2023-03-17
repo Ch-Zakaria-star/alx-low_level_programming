3c1daa469f625d54303574ee00ed5dabba6b3fb0
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-17 16:56:49 -0700

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print all combinations possible of two digits
+ *
+ * Return: 0 if it's true
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
