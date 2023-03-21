df218de0eece8b24373b4b86ba0f04781371bc86
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-21 10:56:27 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print -putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+char s[7] = "_putchar";
+int i;
+
+for (i = 0; i < 8; i++)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+return (0);
+}
