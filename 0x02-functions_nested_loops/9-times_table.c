78d370a483123b2f145dda34bf6f49154a0ba531
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-21 14:05:10 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,31 +8,31 @@
 
 void times_table(void)
 {
-	int r, c, result;
+int r, c, result;
 
-	for (r = 0; r <= 9; r++)
-	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (c = 1; c <= 9; c++)
-		{
-			result = (r * c);
-		if ((result / 10) > 0)
-		{
-			_putchar((result / 10) + '0');
-		}
-		else
-		{
-			_putchar(' ');
-		}
-			_putchar((result % 10) + '0');
-		if (c < 9)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-		_putchar('\n');
-	}
+for (r = 0; r <= 9; r++)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+for (c = 1; c <= 9; c++)
+{
+result = (r * c);
+if ((result / 10) > 0)
+{
+_putchar((result / 10) + '0');
+}
+else
+{
+_putchar(' ');
+}
+_putchar((result % 10) + '0');
+if (c < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
