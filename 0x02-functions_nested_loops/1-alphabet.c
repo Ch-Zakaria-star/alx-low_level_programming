450446bf1a562ff75c45bff1f8ea95c3b8b4e85f
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-21 11:26:08 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print lowercase alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+int a;
+
+for (a = 'a'; a <= 'z'; a++)
+_putchar(a);
+
+putchar('\n');
+}
+
