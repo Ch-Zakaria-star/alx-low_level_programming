3884ab219ddaafea98d90dfd8b588f5946c5e7f0
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-21 12:34:16 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_sign - the absolute value
+ * @n: absolute of a number
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+
+else
+return (-n);
+}
