bd702c46c07ee42e976103ccb4fd1262497f979f
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-22 13:25:05 -0700

    commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
+ * Return: 0.
+ */
+
+int fib(void) {
+int a = 0;
+int b = 1;
+int x = 50;
+int c;
+int i;
+
+for (i = 0; i < x-1; i++) 
+{
+c = b;
+b += a;
+a = c;
+printf("%lu", b);
+
+if (i == 49)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
+}
