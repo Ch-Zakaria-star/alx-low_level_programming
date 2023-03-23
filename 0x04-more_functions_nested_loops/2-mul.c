40bb535ac6939beaf2fec5320a33baf7786b6f82
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-23 13:46:38 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplication.
+ *
+ * @a: first number.
+ * @b: second number.
+ * Return: result.
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
