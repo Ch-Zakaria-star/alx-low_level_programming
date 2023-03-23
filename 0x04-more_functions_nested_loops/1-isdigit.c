9d1c67a0aac4f1b58e381e2ab63c74b29de3d48a
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-23 13:06:12 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check a digit.
+ * @c: characters
+ * Return: 1 and 0
+ */
+
+int _isupper(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+return (0);
+}
