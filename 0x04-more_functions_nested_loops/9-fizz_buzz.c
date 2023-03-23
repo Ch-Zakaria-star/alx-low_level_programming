7714e08dcd3e6b56e50c7df05b641eb07a548cf4
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-23 15:22:10 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - fizzbuzz algorithm
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+int n;
+
+for (n = 1; n < 100; n++)
+{
+if (n % 15 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (n % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", n);
+}
+}
+printf("Buzz\n");
+return (0);
+}
