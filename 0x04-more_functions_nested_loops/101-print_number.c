c41d3577d104e0683870c80d431d99cf1c47abfb
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-23 11:46:11 -0700

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_number - prints an integer n
- *@n: integer to be printed
+ *@n: integer 
  */
 
 void print_number(int n)
@@ -11,9 +11,9 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if ((n / 10) != 0)
+	if (n / 10 != 0)
 	{
 		print_number(n / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar(n % 10 + '0');
 }
