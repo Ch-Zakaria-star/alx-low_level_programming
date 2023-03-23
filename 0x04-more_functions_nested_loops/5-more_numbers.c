53b21f1b8cbdcc9a9323c693a702e59b46c4d6e3
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-23 14:20:03 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0-14.
+ *
+ */
+
+void more_numbers(void)
+{
+int j, i;
+
+j = 0;
+while (j < 10)
+{
+i = 0;
+while (i < 15)
+{
+if (i > 9)
+_putchar(i / 10 + '0');
+
+_putchar(i % 10 + '0');
+i++;
+}
+j++;
+_putchar('\n');
+}
+}
