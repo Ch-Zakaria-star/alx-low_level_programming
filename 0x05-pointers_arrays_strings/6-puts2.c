220baad62c83477459b30774c7e7e4c47e9795f4
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-27 14:23:55 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * puts2 - print every character of a string.
+ * @str: character
+ */
+
+void puts2(char *str)
+{
+int i, j;
+
+for (l = 0; str[j]; j++);
+{
+for (i = 0; i < j; i += 2)
+_putchar(str[i]);
+_putchar('\n');
+}
+}
