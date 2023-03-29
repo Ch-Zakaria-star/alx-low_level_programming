c8f5928dc2004658081686e8168d0dde3ae29429
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-29 06:18:23 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcat - function concatenate two strings
+ * @dest: parameter pointer to a char
+ * @src: parameter pointer to a char
+ * Return: pointer of string dest
+ */
+
+char *_sctract(char *dest, char *src)
+{
+char *conq = dest;
+
+while (*dest != '\0')
+{
+dest++
+}
+while(*src != '\0)
+{
+*dest = *src;
+dest++;
+src++
+}
+*dest = '\0';
+return (conq);
+}
