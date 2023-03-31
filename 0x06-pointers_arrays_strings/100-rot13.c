2a2572a23f2dc5eb25318693c267059e0ee59437
Zakaria Chajia <chajiazakaria@gmail.com>
2023-03-31 04:02:39 -0700

    commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,21 +7,41 @@
  * Return: A pointer to the encoded string.
  */
 
-char *rot13(char *s)
+char *rot13(char *str)
 {
-int i = 0;
+int i = 0, j;
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		       'G', 'H', 'I', 'J', 'K', 'L',
+		       'M', 'N', 'O', 'P', 'Q', 'R',
+		       'S', 'T', 'U', 'V', 'W', 'X',
+		       'Y', 'Z', 'a', 'b', 'c', 'd',
+		       'e', 'f', 'g', 'h', 'i', 'j',
+		       'k', 'l', 'm', 'n', 'o', 'p',
+		       'q', 'r', 's', 't', 'u', 'v',
+		       'w', 'x', 'y', 'z'};
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		       'T', 'U', 'V', 'W', 'X', 'Y',
+		       'Z', 'A', 'B', 'C', 'D', 'E',
+		       'F', 'G', 'H', 'I', 'J', 'K',
+		       'L', 'M', 'n', 'o', 'p', 'q',
+		       'r', 's', 't', 'u', 'v', 'w',
+		       'x', 'y', 'z', 'a', 'b', 'c',
+		       'd', 'e', 'f', 'g', 'h', 'i',
+		       'j', 'k', 'l', 'm'};
 
-while (s[i] != '\0')
+while (str[i])
 {
-while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+for (j = 0; j < 52; j++)
 {
-if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
-s[i] -= 13;
-else
-s[i] += 13;
+if (str[i] == alphabet[j])
+{
+str[i] = rot13key[j];
+break;
+}
+}
+
 i++;
 }
-i++;
-}
-return (s);
+
+return (str);
 }
