dcdad268851b1fdc3ace542900e534f34d266327
Zakaria Chajia <chajiazakaria@gmail.com>
2023-04-03 11:01:46 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: string to be searched
+ * @c: character to be searched in str
+ * Return: number of characters
+ */
+
+char *_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; s[i] != '\0'  ; i++)
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+}
+
+return ('\0');
+}
