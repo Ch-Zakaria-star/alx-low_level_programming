32c49e07015d523ecf8db29a34c3bb6c49bd2ca3
Zakaria Chajia <chajiazakaria@gmail.com>
2023-04-03 13:31:34 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - get length of a prefix substring
+ * @s: string
+ * @accept: bytes being compared
+ * Description: finds number of bytes required to find accept
+ * Return: bytes required to find accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j, spn = 0;
+
+for (i = 0; s[i] >= '\0'; i++)
+{
+for (j = 0; accept[i] > '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+spn++;
+break;
+}
+}
+if (accept[j] == '\0')
+{
+break;
+}
+}
+return (spn);
+}
