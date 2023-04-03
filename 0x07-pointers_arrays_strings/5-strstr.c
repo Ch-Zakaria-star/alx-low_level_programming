3f423147b8db037aafd3baa7ef5252339e7335b5
Zakaria Chajia <chajiazakaria@gmail.com>
2023-04-03 15:51:37 -0700

    commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * _strstr - Locating a substring
- * @needle: Occurance to find
- * @haystack: String to search
- * Return: Pointer
+ * _strncmp - compares two strings within b bytes.
+ * @s1: string to check
+ * @s2: string to check from
+ * @b: number of bytes to check
+ * Return: 0 if strings are the same
+ * any other number if they are different.
  */
 
 char *_strstr(char *haystack, char *needle)
