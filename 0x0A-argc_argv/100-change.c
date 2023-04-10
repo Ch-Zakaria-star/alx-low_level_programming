d1d5742bec5e5d645064cef9a3ac496860ff1ba4
Zakaria Chajia <chajiazakaria@gmail.com>
2023-04-10 10:25:34 -0700

    commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,44 +12,44 @@
 
 int main(int argc, char *argv[])
 {
-int min, r;
+int value, c;
 
-r = 0;
+c = 0;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-min = atoi(argv[1]);
+value = atoi(argv[1]);
 if (value < 0)
 {
 printf("%d\n", 0);
 return (0);
 }
-if (min % 25 >= 0)
+if (value % 25 >= 0)
 {
-r += min / 25;
+c += value / 25;
 value = value % 25;
 }
-if (min % 10 >= 0)
+if (value % 10 >= 0)
 {
-r += min / 10;
-min = min % 10;
+c += value / 10;
+value = value % 10;
 }
-if (min % 5 >= 0)
+if (value % 5 >= 0)
 {
-r += min / 5;
-min = min % 5;
+c += value / 5;
+value = value % 5;
 }
-if (min % 2 >= 0)
+if (value % 2 >= 0)
 {
-r += min / 2;
-min = min % 2;
+c += value / 2;
+value = value % 2;
 }
-if (min % 1 >= 0)
+if (value % 1 >= 0)
 {
-r += min / 1;
+c += value / 1;
 }
-printf("%d\n", r);
+printf("%d\n", c);
 return (0);
 }
