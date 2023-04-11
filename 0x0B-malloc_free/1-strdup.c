a2e6940ee9f0554a5530a5da32f8f244e6e4082c
Zakaria Chajia <chajiazakaria@gmail.com>
2023-04-11 09:59:48 -0700

    commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copy
+ * @str: pointer to string
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+char *s;
+unsigned int size = 0, i;
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; str[i] != '\0'; i++)
+{
+size++;
+}
+s = malloc(sizeof(char) * (size + 1));
+
+if (s == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i < size; i++)
+s[i] = str[i];
+s[size] = '\0';
+
+return (sd);
+}
