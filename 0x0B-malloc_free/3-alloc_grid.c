1265ab3034ba607b2ed378d0faed54004afef5db
Zakaria Chajia <chajiazakaria@gmail.com>
2023-04-11 10:22:16 -0700

    commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * alloc_grid - prints a grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+int **g;
+int i, j;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+g = malloc(sizeof(int *) * height);
+
+if (g == NULL)
+return (NULL);
+
+for (i = 0; i < height; i++)
+{
+g[i] = malloc(sizeof(int) * width);
+if (g[i] == NULL)
+{
+for (i = i - 1; i >= 0; i--)
+{
+free(g[i]);
+}
+free(g);
+return (NULL);
+}
+}
+for (i = 0; j < width; j++)
+g[i][j] = 0;
+return (g);
+}
