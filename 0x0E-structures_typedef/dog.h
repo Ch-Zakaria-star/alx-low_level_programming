61005473a13d27bc49725a6824c29c47a6feb026
Zakaria Chajia <chajiazakaria@gmail.com>
2023-04-17 19:11:37 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,24 @@
-#include <stddef.h>
-#include "dog.h"
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
-* init_dog - inits a variable of
-* type dog
-* @d: dog identification
-* @name: name of dog
-* @age: age of dog
-* @owner: owner's name
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
 */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
