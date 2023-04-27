1328b84e7706461585919e906130747257728505
Zakaria Chajia <chajiazakaria@gmail.com>
2023-04-27 14:37:35 -0700

    commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	current = head;
+	while (current != NULL)
+	{
+		free(current->str);
+		free(current);
+		head = head->next;
+		current = head;
+	}
+}
