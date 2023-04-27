89d614e2c06b61261e5a7c3a852f0298cc765112
Zakaria Chajia <chajiazakaria@gmail.com>
2023-04-27 14:26:40 -0700

    commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: The number of nodes.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
