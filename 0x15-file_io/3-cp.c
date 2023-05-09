b26c02678d8b26c01521a8f77e0629c14c782fca
Zakaria Chajia <chajiazakaria@gmail.com>
2023-05-08 17:55:21 -0700

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,50 +1,87 @@
 #include "main.h"
 
 /**
- * main - cp commnad
- * @argc: argumnet count
- * @argv: argument vector
- * Return: 0 on success
+ * main - Copies the content of a file to another file
+ * @argc: The number of arguments
+ * @argv: The arguments
+ *
+ * Return: 0 if successful, otherwise a number between 97 and
+ * 100 (each number represents an error)
  */
 
 int main(int argc, char *argv[])
 {
-	ssize_t r, o1, o2, c1, c2;
-	char buf[BUFSIZ];
+	int from_fd, to_fd;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	o1 = open(argv[1], O_RDONLY);
-	if (o1 == -1)
+	to_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (to_fd < 0)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	from_fd = open(argv[1], O_RDONLY);
+	if (from_fd < 0)
+	{
+		close_fd(to_fd);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	o2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	copy_contents(from_fd, to_fd, argv[1], argv[2]);
+	close_fd(from_fd), close_fd(to_fd);
+	return (0);
+}
 
-	while ((r = read(o1, buf, BUFSIZ)) > 0)
+/**
+ * close_fd - closes a file handle and exits program on failure
+ * @fd: The file handle
+ */
+
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
 	{
-		if (o2 == -1 || write(o2, buf, r) != r)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(o1);
-			close(o2);
-			exit(99);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
+		exit(100);
 	}
-			if (r == -1)
+}
+
+/**
+ * copy_contents - Copies the contents from one file to another
+ * @from_fd: The source file handle
+ * @to_fd: The destination file handle
+ * @src_file: The source file name
+ * @dest_file: The destination file name
+ */
+
+void copy_contents(int from_fd, int to_fd, char *src_file, char *dest_file)
+{
+	int i, c, buf_size = 1024;
+	void *buf = malloc(sizeof(char) * buf_size);
+
+	if (buf != NULL)
+	{
+		for (i = 0; ; i += buf_size)
+		{
+			c = read(from_fd, buf, buf_size);
+			if (c == 0)
+				break;
+			if (c < 0)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+				dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file);
 				exit(98);
 			}
-			c1 = close(o1), c2 = close(o2);
-			if (c1 == -1 || c2 == -1)
+			if (write(to_fd, buf, c) != c)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", c1 == -1 ? o1 : o2);
-				exit(100);
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_file);
+				free(buf);
+				exit(99);
 			}
-			return (0);
+		}
+		free(buf);
+	}
 }
